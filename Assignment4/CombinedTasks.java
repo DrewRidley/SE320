d5a739a07f41c662df6c7aa019cc4a8262efef5b
Drew Ridley <drew.ridley03@gmail.com>
2023-11-17 17:33:01 -0500

    Assignment4

---
+++ Assignment4/CombinedTasks.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CombinedTasks {
+    public static void main(String[] args) {
+        // Task 1: LinkedHashSet Operations
+        linkedHashsetOps();
+
+        // Task 2: TreeSet for Nonduplicate Words
+        treesetPrinter("words.txt"); // Replace with your file path
+
+        // Task 3: Number Formatting and Parsing
+        formatAndParseNumbers();
+    }
+
+    private static void linkedHashsetOps() {
+        Set<String> set1 = new LinkedHashSet<>(Set.of("George", "Jim", "John", "Blake", "Kevin", "Michael"));
+        Set<String> set2 = new LinkedHashSet<>(Set.of("George", "Katie", "Kevin", "Michelle", "Ryan"));
+
+        // Union
+        Set<String> union = new LinkedHashSet<>(set1);
+        union.addAll(set2);
+
+        // Difference
+        Set<String> difference = new LinkedHashSet<>(set1);
+        difference.removeAll(set2);
+
+        // Intersection
+        Set<String> intersection = new LinkedHashSet<>(set1);
+        intersection.retainAll(set2);
+
+        System.out.println("Union: " + union);
+        System.out.println("Difference: " + difference);
+        System.out.println("Intersection: " + intersection);
+    }
+
+    private static void treesetPrinter(String filePath) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            TreeSet<String> words = new TreeSet<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] wordLine = line.split("\\s+");
+                for (String word : wordLine) {
+                    words.add(word);
+                }
+            }
+            br.close();
+
+            System.out.println("\nNonduplicate words in ascending order:");
+            words.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    private static void formatAndParseNumbers() {
+        double number = 12345.678;
+
+        // UK 
+        NumberFormat nfUK = NumberFormat.getNumberInstance(Locale.UK);
+        nfUK.setMaximumFractionDigits(2);
+        System.out.println("\nFormatted in UK Locale: " + nfUK.format(number));
+
+        // US 
+        NumberFormat nfUS = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("Formatted in US Currency: " + nfUS.format(number));
+
+        // Parsing
+        try {
+            Number parsedNumber = nfUK.parse("12,345.678");
+            System.out.println("Parsed Number: " + parsedNumber);
+        } catch (ParseException e) {
+            System.out.println("Error parsing number: " + e.getMessage());
+        }
+    }
+}
